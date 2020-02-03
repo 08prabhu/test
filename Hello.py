2dfe1bc08f13f0e3cf33fbf8d999cfabb82d4a54
Prabhu S <48690323+08prabhu@users.noreply.github.com>
2020-02-03 14:07:38 +0530

    Performing Arith operation

--- Hello.py
+++ Hello.py
@@ -1,1 +1,5 @@
 print("Hello to the CI test")
+print ("Give 2 numbers for addition operation")
+num1 = float(input(" Please Enter the First Value Number 1: "))
+num2 = float(input(" Please Enter the Second Value Number 2: "))
+print("Result: ", num1+num2)
