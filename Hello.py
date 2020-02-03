174b4fbca74ae0867bc6ebc6ae606d95770e9d5e
Prabhu S <48690323+08prabhu@users.noreply.github.com>
2020-02-03 14:11:35 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,5 +1,5 @@
 print("Hello to the CI test")
 print ("Give 2 numbers for addition operation")
-num1 = float(input(" Please Enter the First Value Number 1: "))
-num2 = float(input(" Please Enter the Second Value Number 2: "))
+num1 = %1
+num2 = %2
 print("Result: ", num1+num2)
